b1b7628b90e7d9644ecfbedd89323b43151a7005
Jacob Olenick <olenickjacob@gmail.com>
2023-08-30 22:06:42 -0700

    added a few more empty blog posts to the main page. Added some styling to the main.

--- teamblog/src/App.js
+++ teamblog/src/App.js
@@ -3,7 +3,9 @@ import Blogcard from './Components/Blogcard';
 
 const blogs = [
   {title: "Why I'm teaching myself React", body: "This is my first blog post!"},
-  {title: "My first week learning ReactJS", body: "This is my first blog post!"}
+  {title: "My first week learning ReactJS", body: "This is my first blog post!"},
+  {title: "I built a simple blog", body: "This is my first blog post!"},
+  {title: "I enjoy coding as a way to feel calm", body: "This is my first blog post!"}
 ];
 
 function App() {
